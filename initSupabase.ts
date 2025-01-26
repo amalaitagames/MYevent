99ef05c1377ec73a548f36ce47d7ecca0bfa46b9
amalaitagames <103190881+amalaitagames@users.noreply.github.com>
2025-01-26 23:47:31 +0100

    Update initSupabase.ts

--- initSupabase.ts
+++ initSupabase.ts
@@ -1,5 +1,5 @@
 import {createClient} from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://wyhjrxktijlgrtfdagsu.supabase.co'
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind5aGpyeGt0aWpsZ3J0ZmRhZ3N1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc0NzM3NjIsImV4cCI6MjA1MzA0OTc2Mn0.Xh-Va-AxMazb2EiJLGFNt4mrdcSqV_ii7w2YuplzL3c";
+const supabaseUrl = 
+const supabaseKey = 
 export const supabase = createClient(supabaseUrl, supabaseKey);
